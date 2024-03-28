0827c4ab163adf2bc7094b52685e2d7fdb79fa98
MickaelSanches <mickael.4870@gmail.com>
2024-03-28 17:19:43 +0100

    install dependencies and create app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+/* eslint-disable linebreak-style */
+const express = require('express');
+
+const app = express();
+
+// Middlewares
+app.use(express.json()); // to parse the JSON
+
+// Routers
+app.get('/', (req, res) => {
+  res.send('Bienvenue sur Cohabit');
+});
+
+// Démarrage du serveur
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Le serveur est fonctionnel sur le port ${PORT}`);
+});
