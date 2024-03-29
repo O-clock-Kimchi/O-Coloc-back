c5a841b9cc4dae584d2c0b8b317eac948404d67b
MickaelSanches <mickael.4870@gmail.com>
2024-03-29 10:38:03 +0100

    test pull

---
+++ app/data/database.js
@@ -0,0 +1,9 @@
+/* eslint-disable linebreak-style */
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('nom_de_la_base_de_donnees', 'utilisateur', 'mot_de_passe', {
+  host: 'localhost',
+  dialect: 'postgres',
+  logging: false,
+  // eslint-disable-next-line eol-last
+});
