c6395588934f8494b339cc5f1526443665c85a93
MickaelSanches <mickael.4870@gmail.com>
2024-04-02 10:26:16 +0200

    creation of models

---
+++ app/models/Colocs.js
@@ -0,0 +1,40 @@
+/* eslint-disable comma-dangle */
+/* eslint-disable linebreak-style */
+// models/Coloc.js
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../data/database');
+
+class Coloc extends Model {}
+
+Coloc.init({
+  coloc_id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false
+  },
+  name: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  groupe_code_valid: {
+    type: DataTypes.TEXT,
+    unique: true
+  },
+  date_creation: {
+    type: DataTypes.DATE,
+    allowNull: false
+  },
+  lien_coloc: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+    unique: true
+  }
+}, {
+  sequelize,
+  modelName: 'Colocs',
+  tableName: 'colocs',
+  timestamps: false
+});
+
+module.exports = Coloc;
---
+++ app/models/Users.js
@@ -0,0 +1,41 @@
+/* eslint-disable comma-dangle */
+/* eslint-disable linebreak-style */
+// models/User.js
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../data/database');
+
+class User extends Model {}
+
+User.init({
+  user_id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false
+  },
+  firstname: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+    unique: true
+  },
+  password: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  color: {
+    type: DataTypes.STRING(7),
+    allowNull: false,
+    unique: true
+  }
+}, {
+  sequelize,
+  modelName: 'Users',
+  tableName: 'users',
+  timestamps: false
+});
+
+module.exports = User;
