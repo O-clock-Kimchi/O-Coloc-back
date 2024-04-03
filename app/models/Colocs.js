de636aebf1864c329b4139f7c0962a8f3e63254c
Caitlyne Bertrandias <caitlyne.bertrandias@oclock.school>
2024-04-03 10:58:00 +0200

    update file Colocs in  models

--- app/models/Colocs.js
+++ app/models/Colocs.js
@@ -24,6 +24,7 @@ Coloc.init({
     },
     date_creation: {
         type: DataTypes.DATE,
+        defaultValue: DataTypes.literal('CURRENT_TIMESTAMP'),
         allowNull: false
     },
     lien_coloc: {
