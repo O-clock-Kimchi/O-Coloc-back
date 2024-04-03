c47a7d6394dce9ec27acb2d5e18a9fb0a51c6f40
Caitlyne Bertrandias <caitlyne.bertrandias@oclock.school>
2024-04-03 15:29:15 +0200

    update file database for sequelize

--- app/data/database.js
+++ app/data/database.js
@@ -1,34 +1,12 @@
 /* eslint-disable eol-last */
 /* eslint-disable comma-dangle */
 /* eslint-disable indent */
-// const { Sequelize } = require('sequelize');
-
-// function getConnexion() {
-//     return new Sequelize(
-//         process.env.DB_NAME,
-//         process.env.DB_USERNAME,
-//         process.env.DB_PASSWORD,
-//         {
-//             define: {
-//                 createdAt: 'created_at',
-//                 updatedAt: 'updated_at',
-//                 underscored: true,
-//             },
-//             host: 'localhost',
-//             dialect: 'postgres',
-//             logging: false,
-//         }
-//     );
-// }
-
-
-// module.exports = getConnexion;
-
 const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
-const sequelize = new Sequelize( process.env.DB_NAME,
+const getConnexion = new Sequelize( process.env.DB_NAME,
            process.env.DB_USERNAME,
-           "moucherons",
+           process.env.DB_PASSWORD,
            {
                define: {
                    createdAt: 'created_at',
@@ -38,8 +16,6 @@ const sequelize = new Sequelize( process.env.DB_NAME,
                host: 'localhost',
                dialect: 'postgres',
                logging: false,
-   // process.env.DB_URL, {
-   // dialect: "postgres"
 });
 
-module.exports = sequelize;
+module.exports = getConnexion;
