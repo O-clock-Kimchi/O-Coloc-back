f6f112598a6f2ea3fc718c7cdef1f57a9464c189
Caitlyne Bertrandias <caitlyne.bertrandias@oclock.school>
2024-04-03 10:45:28 +0200

    test server with database

--- app/data/database.js
+++ app/data/database.js
@@ -1,24 +1,45 @@
 /* eslint-disable eol-last */
 /* eslint-disable comma-dangle */
 /* eslint-disable indent */
+// const { Sequelize } = require('sequelize');
+
+// function getConnexion() {
+//     return new Sequelize(
+//         process.env.DB_NAME,
+//         process.env.DB_USERNAME,
+//         process.env.DB_PASSWORD,
+//         {
+//             define: {
+//                 createdAt: 'created_at',
+//                 updatedAt: 'updated_at',
+//                 underscored: true,
+//             },
+//             host: 'localhost',
+//             dialect: 'postgres',
+//             logging: false,
+//         }
+//     );
+// }
+
+
+// module.exports = getConnexion;
+
 const { Sequelize } = require('sequelize');
 
-function getConnexion() {
-    return new Sequelize(
-        process.env.DB_NAME,
-        process.env.DB_USERNAME,
-        process.env.DB_PASSWORD,
-        {
-            define: {
-                createdAt: 'created_at',
-                updatedAt: 'updated_at',
-                underscored: true,
-            },
-            host: 'localhost',
-            dialect: 'postgres',
-            logging: false,
-        }
-    );
-}
+const sequelize = new Sequelize( process.env.DB_NAME,
+           process.env.DB_USERNAME,
+           "moucherons",
+           {
+               define: {
+                   createdAt: 'created_at',
+                   updatedAt: 'updated_at',
+                   underscored: true,
+               },
+               host: 'localhost',
+               dialect: 'postgres',
+               logging: false,
+   // process.env.DB_URL, {
+   // dialect: "postgres"
+});
 
-module.exports = getConnexion;
+module.exports = sequelize;
