9159c6d2d4cd2c28049fc7e4918ceb59d8c5b6bb
audeangelo <audeangelo.aa@gmail.com>
2024-04-04 15:38:30 +0200

    commit aude before merge

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
-/* eslint-disable import/extensions */
 /* eslint-disable linebreak-style */
 const express = require('express');
 const session = require('express-session');
+const cors = require('cors');
+
 const app = express();
-
+const router = require('./app/routers/testRouter');
 const router = require('./app/routers/router');
-
+const router= require('./app/colocRouter');
 require('dotenv').config();
+
+// Configuration de CORS
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Méthodes HTTP autorisées
+    allowedHeaders: ['Content-Type', 'Authorization'], // En-têtes autorisés
+}));
 
 // Middleware pour parser le body des requêtes en JSON
 app.use(express.json());
@@ -15,7 +23,6 @@ app.use(express.json());
 app.get('/', (_, res) => {
     res.send('Bienvenue sur Cohabit!');
 });
-
 
 // Configuration du middleware express-session
 app.use(session({
@@ -27,10 +34,9 @@ app.use(session({
 app.use(router);
 
 // Synchronisation des modèles avec la base de données et démarrage du serveur
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 5000);
 app.set('base_url', process.env.BASE_URL || 'http://localhost');
 
 app.listen(app.get('port'), () => {
     console.log(`Listening on ${app.get('base_url')}:${app.get('port')}`);
 });
-console.log(process.env.DB_USERNAME)
