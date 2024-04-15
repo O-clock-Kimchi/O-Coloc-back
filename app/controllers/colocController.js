33684288ce2c309bd050360f6869d9df6ceb6abd
Caitlyne Bertrandias <caitlyne.bertrandias@oclock.school>
2024-04-15 11:12:39 +0200

    update generateNewCode

--- app/controllers/colocController.js
+++ app/controllers/colocController.js
@@ -189,28 +189,27 @@ const colocController = {
 
     },
     async generateNewCode(req, res) {
-    try {
-      const id = req.params.id;
-      const coloc = await Colocs.findByPk(id);
-      if (coloc) {
-        const code = Math.floor(10000000 + Math.random() * 90000000)
-          .toString()
-          .substring(0, 8);
-        await coloc.update({ lien_coloc: code, groupe_code_valid: code });
-        res.status(200).json({ message: 'Changement de code effectué' });
-      } else {
-        console.error(
-          'Colocation non trouvée lors de la génération du code pour la colocation :',
-          id
-        );
-      }
-    } catch (error) {
-      console.error(
-        'Erreur lors de la génération du code pour la colocation :',
-        error
-      );
-    }
-  }
+        try {
+          const id = req.params.id;
+          const coloc = await Colocs.findByPk(id);
+          if (coloc) {
+            const code = Math.floor(10000000 + Math.random() * 90000000)
+              .toString()
+              .substring(0, 8);
+            await coloc.update({ lien_coloc: code, groupe_code_valid: code });
+            res
+              .status(200)
+              .json({ message: "Changement de code effectué", newCode: code });
+          } else {
+            res.status(404).json({
+              message:
+                "Colocation non trouvée lors de la génération du code pour la colocation ",
+            });
+          }
+        } catch (error) {
+          res.status(500).json({ message: "Une erreur est survenue" });
+        }
+    },
 };
 
 module.exports = colocController;
