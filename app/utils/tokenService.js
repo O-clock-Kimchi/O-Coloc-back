6ec040762b7956c92094648d6d021495e4dee4a4
Caitlyne Bertrandias <caitlyne.bertrandias@oclock.school>
2024-04-19 14:56:12 +0200

    one hour token

--- app/utils/tokenService.js
+++ app/utils/tokenService.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 function generateAccessToken(user_id) {
-    return jwt.sign({ user_id }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1m' });
+    return jwt.sign({ user_id }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
 }
 
 
